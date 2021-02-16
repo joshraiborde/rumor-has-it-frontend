005607493197ff98bbaba42e931d5a4b2e417da6
joshsrai <joshsrai@gmail.com>
2021-02-16 16:59:44 -0500

    comment form

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ postForm.addEventListener("submit", submitPost)
 
 function submitPost() {
     event.preventDefault()
-    console.log(postInput.value)
     const li = document.createElement('li') // contains the info from the post and comment form.
 
     const p = document.createElement('p')
@@ -15,11 +14,29 @@ function submitPost() {
 
     // comment form element
     const commentForm = document.createElement('form')
-    commentForm.innerHTML += `<input type="text"><input type="submit">`
-    li.append(p,  commentForm)
+    commentForm.innerHTML += `<input type="text" id="comment-input"><input type="submit">`
+    commentForm.addEventListener("submit", submitComment)
+
+    const commentList = document.createElement('ul')
+
+    li.append(p,  commentForm, commentList)
 
     postList.appendChild(li)
 
+    postForm.reset()
+
+}
+
+function submitComment(e){ // using e.target can identify which form a comment goes into
+    e.preventDefault()
+    const commentInput = e.target.children[0].value
+    const commentList = e.target.nextElementSibling
+
+    const li = document.createElement('li')
+    li.innerText = commentInput
+    commentList.appendChild(li)
+
+    e.target.reset() // in the comment form, this will reset the input slot, after the submit button is clicked
 }
 
  

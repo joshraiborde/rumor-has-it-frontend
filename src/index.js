28cd3b61715c75b6d49a996e133b6959a7fde6ed
joshsrai <joshsrai@gmail.com>
2021-02-16 16:37:16 -0500

    added post form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Rumor has it...")
+const postForm = document.getElementById("post-form")
+const postInput = document.getElementById("post-input")
+const postList = document.getElementById("post-list")
+
+postForm.addEventListener("submit", submitPost)
+
+function submitPost() {
+    event.preventDefault()
+    console.log(postInput.value)
+    const li = document.createElement('li') // contains the info from the post and comment form.
+
+    const p = document.createElement('p')
+    p.innerText = postInput.value
+    
+
+    // comment form element
+    const commentForm = document.createElement('form')
+    commentForm.innerHTML += `<input type="text"><input type="submit">`
+    li.append(p,  commentForm)
+
+    postList.appendChild(li)
+
+}
+
+ 

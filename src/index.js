113c9158425773ad3aaff3cd5e0e8bbbc9e6f5d9
joshsrai <joshsrai@gmail.com>
2021-02-17 08:58:59 -0500

    added fectchPosts, added to submitPosts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const postForm = document.getElementById("post-form")
 const postInput = document.getElementById("post-input")
 const postList = document.getElementById("post-list")
+const postURL= `http://localhost:3000/posts`
+
+
+function fetchPosts(){
+    fetch(postURL)
+    .then(res => res.json())
+    .then(posts => posts.forEach(post => renderPost(post.content)))
+}
 
 postForm.addEventListener("submit", submitPost)
 
+// fetch request
 function submitPost() {
     event.preventDefault()
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept-type": "application/json"
+        },
+        body: JSON.stringify({
+            content: postInput.value
+        })
+    }
+    fetch(postURL, configObj)
+
+    renderPost(postInput.value)
 }
  
 // render post to dom
@@ -43,4 +65,7 @@ function submitComment(e){ // using e.target can identify which form a comment g
     e.target.reset() // in the comment form, this will reset the input slot, after the submit button is clicked
 }
 
- 
+fetchPosts()
+
+ // stopped at 00:47:00
+ // page 6, number 70

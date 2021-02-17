7e5017ea995ebc6d70a67659d237d791beb789a5
joshsrai <joshsrai@gmail.com>
2021-02-17 14:28:19 -0500

    created a post class and added a constructor, created and-or  moved over renderPosts, fetchPosts, renderPost, submitPost and deletePost in the post js file

--- src/post.js
+++ src/post.js
@@ -9,6 +9,79 @@ class Post{
         Post.allPosts.push(this)
     }
 
-    
+    static renderPosts(){
+        for(let post of this.allPosts){
+            post.renderPost()
+        }
+    }
 
+    static fetchPosts(){
+        fetch(postURL)
+        .then(res => res.json())
+        .then(posts => {
+            for(let post of posts) {
+                let newlist = new Post(post.data)
+            }
+            this.renderPosts()
+        })
+    }
+
+    renderPost(){
+        const li = document.createElement('li')
+        li.dataset.id = this.id
+
+        const p = document.createElement('p')
+        p.innerText = this.content
+
+        const deleteBtn = document.createElement("button")
+        deleteBtn.innerText = "delete"
+        deleteBtn.addEventListener("click", this.deletePost)
+
+        const commentForm = document.createElement('form')
+        commentForm.innerHTML += `<input type="text" id="comment-input"><input type="submit">`
+        commentForm.addEventListener("submit", Comment.createComment)
+
+        const commentList = document.createElement('ul')
+        this.comments.forEach(comment => {
+            let commentObj = new Comment(comment)
+            commentObj.renderComment(commentList)
+        })
+
+        li.append(p, deleteBtn, commentForm, commentList)
+
+        postList.appendChild(li)
+
+        postForm.rest()
+    }
+
+    static submitPost(){
+        event.defaultPrevented()
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "Accept-type": "application/json"
+            },
+            body: JSON.stringify({
+                content: postInput.value
+            })
+        }
+
+        fetch(postURL, configObj)
+        .then(res => res.json())
+        .then(data => {
+            let newPost= new Post(data.data)
+            newPost.renderPost()
+        })
+    }
+
+    deletePost(){
+        const postId = this.parentElement.dataset.id
+
+        fetch(`${postUrl}/${postId}`, {
+            method: "DELETE"
+        })
+        this.parentElement.remove()
+    }
+    
 }
